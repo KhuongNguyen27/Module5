f886b6d7b5b115552458cb173d1f859243eb6e71
nguyenhuukhuong27102000@gmail.com <nguyenhuukhuong27102000@gmail.com>
2023-08-21 16:39:01 +0700

    first commit

---
+++ TypeScript/test.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+    console.log('LẬP');
+    setTimeout(function () {
+        console.log('TRÌNH');
+        setTimeout(function () {
+            console.log('CODEGYM');
+        }, 2000);
+    }, 3000);
+}, 5000);
